581f1640ad9a3d137a65ef855a979733dd82685f
Sergei Leonov <sleonov27@amherst.edu>
2025-11-20 18:31:54 -0500

    refactoring the server code structure

--- server.py
+++ server.py
@@ -2,54 +2,60 @@ import socket
 import threading
 import argparse
 
-# define globals
-document = ""
-version = 0
-clients = []
-data_lock = threading.Lock()
+class Server(object):
+    def __init__(self, host, port):
+        # define instance vars
+        self.doc = ""
+        self.doc_ver = 0
+        self.clients = []
+        self.data_lock = threading.Lock()
 
-def connection_listener(server_socket):
-    # listen for new connections
-    while True:
-        # get ip and port
-        client_socket, addr = server_socket.accept() 
+        # bind socket to ip with given port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((host, port))
 
-        # store client data in list
-        global clients
-        clients.append((client_socket, addr))
+        self.server_socket.listen()
+        print(f"Server listening on {host}:{port}...")
+
+    def connection_listener(self):
+        # listen for new connections
+        while True:
+            # get ip and port
+            client_socket, addr = self.server_socket.accept() 
+
+            # store client data in list
+            self.clients.append((client_socket, addr))
+            
+            # start new thread for newly connected client
+            thread = threading.Thread(target=self.connection_handler, args=(client_socket, addr))
+            thread.start()
+
+            # TODO: periodically send updated file to each client
+
+    def connection_handler(self, client_socket, addr):
+        print(f"(New Thread) Connected by {addr}")
+        local_ip, local_port = client_socket.getsockname()
+        print(f"Using IP {local_ip} and port {local_port} for this client")
+
+        # receive data and process into cmd code and url
+        while True:
+            # TODO: process data from client according to the packet received
+            data = client_socket.recv(1460)
         
-        # start new thread for newly connected client
-        thread = threading.Thread(target=connection_handler, args=(client_socket, addr))
-        thread.start()
+        client_socket.close()
 
-        # TODO: periodically send updated file to each client
+    def write_file(self, filename="server_file.txt"):
+        with open(filename, 'w') as f:
+            # writes the lines into a file on disk 
+            f.writelines(self.doc)
 
-def connection_handler(client_socket, addr):
-    print(f"(New Thread) Connected by {addr}")
-    local_ip, local_port = client_socket.getsockname()
-    print(f"Using IP {local_ip} and port {local_port} for this client")
-
-    # receive data and process into cmd code and url
-    while True:
-        # TODO: process data from client according to the packet received
-        data = client_socket.recv(1460)
-    
-    client_socket.close()
-
-def write_file(filename="server_file.txt"):
-    with open(filename, 'w') as f:
-        global document
-        # writes the lines into a file on disk 
-        f.writelines(document)
-
-def open_file(filename="server_file.txt"):
-    try:
-        with open(filename, 'r') as f:
-            global document
-            # obtains a list of lines as strings in a file (includes terminating \n)
-            document = f.readlines()
-    except FileNotFoundError:
-        print("File not found...")
+    def open_file(self, filename="server_file.txt"):
+        try:
+            with open(filename, 'r') as f:
+                # obtains a list of lines as strings in a file (includes terminating \n)
+                self.doc = f.readlines()
+        except FileNotFoundError:
+            print("File not found...")
 
 
 def main():
@@ -63,22 +69,17 @@ def main():
     HOST = args.host
     PORT = int(args.port)
 
-    # bind socket to ip with given port
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((HOST, PORT))
-
-    server_socket.listen()
-    print(f"Server listening on {HOST}:{PORT}...")
+    server = Server(HOST, PORT)
 
     # start a listener thread for the server
     try:
-        main_thread = threading.Thread(target=connection_listener, args=(server_socket,), daemon=True, name="main_thread")
+        main_thread = threading.Thread(target=server.connection_listener, daemon=True, name="main_thread")
         main_thread.start()
         main_thread.join()
     except KeyboardInterrupt:
         print("\nShutting down server...")
     finally:
-        server_socket.close()
+        server.server_socket.close()
         print("Done.")
 
 if __name__ == "__main__":
